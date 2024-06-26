b4d66ba9ceccdae1dbf99cdf7b8a895a8d820b5c
Noemi <45180344+unflxw@users.noreply.github.com>
2024-06-26 15:49:23 +0200

    Add some behaviour to /api

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,6 +1,9 @@
 export async function GET(request: Request) {
   console.log("/api route handler called!")
-  return new Response('Hello, Next.js!', {
+
+  const response = await fetch("https://api.github.com/users/octocat")
+  const json = await response.json()
+  return new Response(`Hello, ${json.name}!`, {
     status: 200
   })
 }
